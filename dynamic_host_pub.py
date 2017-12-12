5b8fd64726412e1506438053889b238eb40f260c
Steve Woolley <steve.woolley@me.com>
2017-12-12 15:28:57 -0500

    added dynamic host query

---
+++ dynamic_host_pub.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import awsiot
+import logging
+import platform
+import psutil
+import subprocess as sp
+
+STATE = 'state'
+REPORTED = 'reported'
+DESIRED = 'desired'
+THING_SHADOW = "$aws/things/{}/shadow/update"
+DATE_FORMAT = '%Y/%m/%d %-I:%M %p %Z'
+
+
+def os_execute(s):
+    """Returns string result of os call"""
+    try:
+        result = sp.check_output(s.split()).rstrip('\n')
+        return result
+    except Exception as ex:
+        return None
+
+
+def get_rpi_cpu_temperature():
+    """Returns raspberry pi cpu temperature in Centigrade"""
+    temp = os_execute('/opt/vc/bin/vcgencmd measure_temp')
+    return float(temp.split('=')[1].strip('\'C'))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", required=True, help="Your AWS IoT custom endpoint")
+    parser.add_argument("-r", "--rootCA", required=True, help="Root CA file path")
+    parser.add_argument("-c", "--cert", required=True, help="Certificate file path")
+    parser.add_argument("-k", "--key", required=True, help="Private key file path")
+    parser.add_argument("-n", "--thing", help="Targeted thing name")
+
+    parser.add_argument("-g", "--groupCA", default=None, help="Group CA file path")
+    parser.add_argument("-m", "--mqttHost", default=None, help="Targeted mqtt host")
+
+    parser.add_argument("-t", "--topic", default="/test", help="Targeted topic")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    args = parser.parse_args()
+
+    publisher = awsiot.Publisher(args.endpoint, args.rootCA, args.cert, args.key, args.thing, args.groupCA)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        publisher.log_level = logging.DEBUG
+
+    properties = {}
+    mem = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
+    if platform.machine().startswith('arm') and platform.system() == 'Linux':  # raspberry pi
+        properties["cpuTemp"] = get_rpi_cpu_temperature()
+    properties["ramAvailable"] = int(mem.available / (1024 * 1024))
+    properties["usedDiskSpaceRoot"] = int(disk.used / (1024 * 1024))
+    properties["cpuLoad"] = psutil.cpu_percent(interval=3)
+
+    topic = THING_SHADOW.format(args.thing)
+    payload = json.dumps({STATE: {REPORTED: properties}})
+    result = publisher.publish(topic, payload)
