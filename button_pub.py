bd3004c1fb7b2f6272e360dcff9249f85f4a115c
Steve Woolley <steve.woolley@me.com>
2017-12-12 22:17:32 -0500

    added button and motion sensor

---
+++ button_pub.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import awsiot
+import logging
+from gpiozero import Button
+from signal import pause
+
+
+def pressed():
+    logging.info("button_pub: button pressed on pin: {}".format(args.pin))
+    message_json = json.dumps({'button': True})
+    for t in args.topic:
+        publisher.publish(t, message_json)
+
+
+def released():
+    logging.info("button_sub: button_released on pin: {}".format(args.pin))
+    message_json = json.dumps({'button': False})
+    for t in args.topic:
+        publisher.publish(t, message_json)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", required=True, help="Your AWS IoT custom endpoint")
+    parser.add_argument("-r", "--rootCA", required=True, help="Root CA file path")
+    parser.add_argument("-c", "--cert", required=True, help="Certificate file path")
+    parser.add_argument("-k", "--key", required=True, help="Private key file path")
+    parser.add_argument("-n", "--thing", help="Targeted thing name")
+
+    parser.add_argument("-g", "--groupCA", default=None, help="Group CA file path")
+    parser.add_argument("-m", "--mqttHost", default=None, help="Targeted mqtt host")
+
+    parser.add_argument("-t", "--topic", help="MQTT topic(s)", nargs='+', required=False)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+    parser.add_argument("-p", "--pin", help="gpio pin (using BCM numbering)", type=int, required=True)
+    parser.add_argument("-u", "--pull_up",
+                        help="If True (the default), the GPIO pin will be pulled high by default. " +
+                             "In this case, connect the other side of the button to ground. " +
+                             "If False, the GPIO pin will be pulled low by default. " +
+                             "In this case, connect the other side of the button to 3V3",
+                        type=bool, default=True)
+    parser.add_argument("-b", "--bounce_time",
+                        help="If None (the default), no software bounce compensation will be performed. " +
+                             "Otherwise, this is the length of time (in seconds) " +
+                             "that the component will ignore changes in state after an initial change.",
+                        type=float, default=None)
+    args = parser.parse_args()
+
+    publisher = awsiot.Publisher(args.endpoint, args.rootCA, args.cert, args.key)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        publisher.log_level = logging.DEBUG
+
+    pir = Button(args.pin, pull_up=args.pull_up, bounce_time=args.bounce_time)
+
+    pir.when_pressed = pressed
+    pir.when_released = released
+
+    pause()
---
+++ motion_pub.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import awsiot
+import logging
+from gpiozero import MotionSensor
+from signal import pause
+
+
+def motion():
+    logging.info("motion_pub: motion detected on pin: {}".format(args.pin))
+    message_json = json.dumps({'motion': True})
+    for t in args.topic:
+        publisher.publish(t, message_json)
+
+
+def no_motion():
+    logging.info("motion_sub: no motion detected on pin: {}".format(args.pin))
+    message_json = json.dumps({'motion': False})
+    for t in args.topic:
+        publisher.publish(t, message_json)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", required=True, help="Your AWS IoT custom endpoint")
+    parser.add_argument("-r", "--rootCA", required=True, help="Root CA file path")
+    parser.add_argument("-c", "--cert", required=True, help="Certificate file path")
+    parser.add_argument("-k", "--key", required=True, help="Private key file path")
+    parser.add_argument("-n", "--thing", help="Targeted thing name")
+
+    parser.add_argument("-g", "--groupCA", default=None, help="Group CA file path")
+    parser.add_argument("-m", "--mqttHost", default=None, help="Targeted mqtt host")
+
+    parser.add_argument("-t", "--topic", help="MQTT topic(s)", nargs='+', required=False)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+    parser.add_argument("-p", "--pin", help="gpio pin (using BCM numbering)", type=int, required=True)
+    parser.add_argument("-q", "--queue_len",
+                        help="The length of the queue used to store values read from the sensor. (1 = disabled)",
+                        type=int, default=1)
+    parser.add_argument("-x", "--sample_rate",
+                        help="The number of values to read from the device (and append to the internal queue) per second",
+                        type=float, default=100)
+    parser.add_argument("-y", "--threshold",
+                        help="When the mean of all values in the internal queue rises above this value, the sensor will be considered active by the is_active property, and all appropriate events will be fired",
+                        type=float, default=0.5)
+    args = parser.parse_args()
+
+    publisher = awsiot.Publisher(args.endpoint, args.rootCA, args.cert, args.key)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        publisher.log_level = logging.DEBUG
+
+    pir = MotionSensor(args.pin,
+                       queue_len=args.queue_len,
+                       sample_rate=args.sample_rate,
+                       threshold=args.threshold)
+
+    pir.when_motion = motion
+    pir.when_no_motion = no_motion
+
+    pause()
