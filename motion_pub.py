7206008c890d3b4e7b3c2eed234031933a74bb07
Steve Woolley <steve.woolley@me.com>
2017-12-14 11:38:56 -0500

    added glabal variable moving to keep from saturating with events

--- motion_pub.py
+++ motion_pub.py
@@ -7,27 +7,35 @@ import logging
 from gpiozero import MotionSensor
 from signal import pause
 
+moving = False
+
 
 def motion():
-    logging.info("{} {} detected".format(args.source, args.pin))
-    message = {args.source: args.high_value}
-    if args.topic is not None:
-        message[awsiot.MESSAGE] = "{} {}".format(args.source, args.high_value)
-        for t in args.topic:
-            publisher.publish(t, json.dumps(args.source, message))
-    if args.thing is not None:
-        publisher.publish(awsiot.iot_thing_topic(args.thing), awsiot.iot_payload(awsiot.REPORTED, message))
+    global moving
+    if not moving:
+        moving = True
+        logging.info("{} {} detected".format(args.source, args.pin))
+        message = {args.source: args.high_value}
+        if args.topic is not None:
+            message[awsiot.MESSAGE] = "{} {}".format(args.source, args.high_value)
+            for t in args.topic:
+                publisher.publish(t, json.dumps(args.source, message))
+        if args.thing is not None:
+            publisher.publish(awsiot.iot_thing_topic(args.thing), awsiot.iot_payload(awsiot.REPORTED, message))
 
 
 def no_motion():
-    logging.info("{} {} ended".format(args.source, args.pin))
-    message = {args.source: args.low_value}
-    if args.topic is not None:
-        message[awsiot.MESSAGE] = "{} {}".format(args.source, args.low_value)
-        for t in args.topic:
-            publisher.publish(t, json.dumps(args.source, message))
-    if args.thing is not None:
-        publisher.publish(awsiot.iot_thing_topic(args.thing), awsiot.iot_payload(awsiot.REPORTED, message))
+    global moving
+    if moving:
+        moving = False
+        logging.info("{} {} ended".format(args.source, args.pin))
+        message = {args.source: args.low_value}
+        if args.topic is not None:
+            message[awsiot.MESSAGE] = "{} {}".format(args.source, args.low_value)
+            for t in args.topic:
+                publisher.publish(t, json.dumps(args.source, message))
+        if args.thing is not None:
+            publisher.publish(awsiot.iot_thing_topic(args.thing), awsiot.iot_payload(awsiot.REPORTED, message))
 
 
 if __name__ == "__main__":
@@ -50,6 +58,7 @@ if __name__ == "__main__":
     parser.add_argument("-y", "--high_value", help="high value", default=1)
     parser.add_argument("-z", "--low_value", help="low value", default=0)
     args = parser.parse_args()
+    moving = False
 
     logging.basicConfig(filename=awsiot.LOG_FILE, level=args.log_level, format=awsiot.LOG_FORMAT)
 
