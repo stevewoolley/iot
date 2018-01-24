d0426ec4047b67908c40d021cc6f43d82af5942c
Steve Woolley <steve.woolley@me.com>
2018-01-24 12:53:25 -0500

    rework

--- supervisor_sub.py
+++ supervisor_sub.py
@@ -15,34 +15,34 @@ def callback(client, user_data, message):
     except ValueError:
         msg = None
     logging.debug("received {} {}".format(message.topic, msg))
-    for x in args.topic:
-        if message.topic.startswith(x):
-            commands = filter(None, message.topic.replace(x, '').split('/'))
-            if len(commands) > 0:
-                cmd = commands.pop(0)
-                if cmd == 'getAllProcessInfo':
-                    logging.debug("command: {}".format(cmd))
-                    results = proxy.supervisor.getAllProcessInfo()
-                    logging.info("getAllProcessInfo {}".format(results))
-                    if args.thing:
-                        supervised = []
-                        for s in results:
-                            supervised.append('{} ({})'.format(s['name'], s['statename']))
-                        publisher = awsiot.Publisher(args.endpoint, args.rootCA, args.cert, args.key)
-                        publisher.publish(awsiot.iot_thing_topic(args.thing),
-                                          awsiot.iot_payload(awsiot.REPORTED, {'supervised': ', '.join(supervised)}))
-                elif cmd == 'startProcess':
-                    logging.debug("command: {}".format(cmd))
-                    cmd = commands.pop(0)
-                    results = proxy.supervisor.startProcess(cmd)
-                elif cmd == 'stopProcess':
-                    logging.debug("command: {}".format(cmd))
-                    cmd = commands.pop(0)
-                    results = proxy.supervisor.stopProcess(cmd)
-                else:
-                    logging.warning('Unrecognized command: {}'.format(cmd))
-            else:
-                logging.warning("No commands: {}".format(message.topic))
+    # for x in args.topic:
+    #     if message.topic.startswith(x):
+    #         commands = filter(None, message.topic.replace(x, '').split('/'))
+    #         if len(commands) > 0:
+    #             cmd = commands.pop(0)
+    #             if cmd == 'getAllProcessInfo':
+    #                 logging.debug("command: {}".format(cmd))
+    #                 results = proxy.supervisor.getAllProcessInfo()
+    #                 logging.info("getAllProcessInfo {}".format(results))
+    #                 if args.thing:
+    #                     supervised = []
+    #                     for s in results:
+    #                         supervised.append('{} ({})'.format(s['name'], s['statename']))
+    #                     publisher = awsiot.Publisher(args.endpoint, args.rootCA, args.cert, args.key)
+    #                     publisher.publish(awsiot.iot_thing_topic(args.thing),
+    #                                       awsiot.iot_payload(awsiot.REPORTED, {'supervised': ', '.join(supervised)}))
+    #             elif cmd == 'startProcess':
+    #                 logging.debug("command: {}".format(cmd))
+    #                 cmd = commands.pop(0)
+    #                 results = proxy.supervisor.startProcess(cmd)
+    #             elif cmd == 'stopProcess':
+    #                 logging.debug("command: {}".format(cmd))
+    #                 cmd = commands.pop(0)
+    #                 results = proxy.supervisor.stopProcess(cmd)
+    #             else:
+    #                 logging.warning('Unrecognized command: {}'.format(cmd))
+    #         else:
+    #             logging.warning("No commands: {}".format(message.topic))
 
 
 if __name__ == "__main__":
@@ -60,8 +60,12 @@ if __name__ == "__main__":
 
     if args.topic is not None and len(args.topic) > 0:
         for t in args.topic:
-            subscriber.subscribe("{}/#".format(t), callback)
-            time.sleep(2)  # pause
+            ary = t.split('/')
+            while len(ary) > 0:
+                prefix = "{}/#".format('/'.join(ary))
+                subscriber.subscribe(prefix, callback)
+                time.sleep(2)  # pause
+                ary.pop()
 
     # Loop forever
     try:
